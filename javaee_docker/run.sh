d83ce9f783c2d4b2a86e12f896efc17655356f4f
xavierdpt <xavierdpt>
2019-02-07 18:47:54 +0100

    .

--- javaee_docker/run.sh
+++ javaee_docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --network host youpi /bin/bash
+docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v workspace:/home/user/workspace youpi
--- javaee_docker/static/init.sh
+++ javaee_docker/static/init.sh
@@ -5,5 +5,5 @@ apt-get update
 apt-get upgrade
 apt-get install -y git firefox
 useradd -m user
-#su -c "DISPLAY=:0 firefox" -l user
+su -c "PATH=/downloaded/apache-maven-3.6.0/bin:/downloaded/apache-tomcat-9.0.14/bin:/downloaded/jdk-11.0.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/user/maven-3.5.3/bin:\"$PATH\" DISPLAY=:0 /downloaded/eclipse-installer/eclipse-inst" -l user
 su -c "PATH=/downloaded/apache-maven-3.6.0/bin:/downloaded/apache-tomcat-9.0.14/bin:/downloaded/jdk-11.0.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/user/maven-3.5.3/bin:\"$PATH\" DISPLAY=:0 /downloaded/eclipse-installer/eclipse-inst" -l user
